11afc5a8bbc0b76ef7e46d61d10fc79284e1407d
Vlad Eliseykin <veliseykin2000@gmail.com>
2024-10-11 18:03:23 -0700

    fix

--- app/src/views/sendForm.vue
+++ app/src/views/sendForm.vue
@@ -2666,6 +2666,11 @@ if (this.currentStep === 2) {
     async loadFormData(device_id) {
   console.log('Загрузка данных формы для device_id:', device_id);
 
+  if (!device_id) {
+    console.error('device_id не передан в loadFormData.');
+    return;
+  }
+
   try {
     const response = await fetch(`http://test.yocto.vc/api/form-response/${device_id}`, {
       method: 'GET',
@@ -2675,7 +2680,6 @@ if (this.currentStep === 2) {
     });
 
     if (response.status === 404) {
-      // Если сервер вернул 404, данных для данного device_id нет
       console.log('Данные для device_id не найдены, начинаем с пустой формы.');
       return;
     }
@@ -2684,6 +2688,7 @@ if (this.currentStep === 2) {
       console.error('Ошибка при загрузке данных формы:', response.status);
       throw new Error('Failed to load form data');
     }
+
     const result = await response.json();
 
     console.log('Данные формы, полученные с сервера:', result);
@@ -2810,9 +2815,13 @@ scrollToCurrentStep() {
     console.log('Существующий device_id найден:', deviceId);
   }
 
-  // Загружаем данные формы для текущего device_id
-  console.log('Текущий device_id:', deviceId);
-  this.loadFormData(deviceId);
+   // Проверяем, что deviceId не равен null или undefined
+   if (deviceId) {
+    console.log('Текущий device_id перед загрузкой данных формы:', deviceId);
+    this.loadFormData(deviceId);
+  } else {
+    console.error('device_id не найден после проверки localStorage.');
+  }
   
     setTimeout(() => {
       this.showTitle = true;
