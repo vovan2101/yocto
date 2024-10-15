2ad83362e86a3127b662a25bd0de9a844c356a14
Vlad Eliseykin <veliseykin2000@gmail.com>
2024-10-15 16:51:18 -0700

    fix

--- app/src/components/FormSelector.vue
+++ app/src/components/FormSelector.vue
@@ -260,7 +260,7 @@ computed: {
     };
 
     try {
-      const response = await fetch('http://localhost:3002/form-response', {
+      const response = await fetch('http://test.yocto.vc/form-response', {
         method: 'POST',
         headers: {
           'Content-Type': 'application/json',
@@ -289,7 +289,7 @@ computed: {
     }
 
     try {
-      const response = await fetch(`http://localhost:3002/form-response/device/${deviceId}`, {
+      const response = await fetch(`http://test.yocto.vc/form-response/device/${deviceId}`, {
         method: 'GET',
         headers: {
           'Content-Type': 'application/json',
