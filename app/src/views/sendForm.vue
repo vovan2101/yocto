14d0b08d2a3b56f327f623d720fe40c097a759e2
vovan2101 <veliseykin2000@gmail.com>
2024-10-20 01:24:39 +0000

    ui fix

--- app/src/views/sendForm.vue
+++ app/src/views/sendForm.vue
@@ -3168,7 +3168,7 @@ button:focus {
   border: 2px solid transparent; /* Убираем рамку */
   cursor: pointer;
   position: relative;
-  width: 100%;
+  width: 96%;
   transition: background-color 0.3s, border-color 0.3s; /* Плавные переходы */
 }
 
