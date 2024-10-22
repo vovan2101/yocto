5fbfaf3db41ed5b5d9436461495d4b17755dd63f
vovan2101 <veliseykin2000@gmail.com>
2024-10-22 17:49:55 +0000

    fix

--- bot/src/app.js
+++ bot/src/app.js
@@ -26,10 +26,10 @@ app.use(cors(corsOptions));
 app.use(bodyParser.json());
 
 // Подключаем маршруты
-app.use(routes);
+app.use('/api', routes);
 
 // Маршрут для отправки форм
-app.post('/send-forms', upload.fields([
+app.post('/api/send-forms', upload.fields([
   { name: 'pitch_deck_file', maxCount: 1 }
 ]), (req, res) => {
   const formData = req.body;
