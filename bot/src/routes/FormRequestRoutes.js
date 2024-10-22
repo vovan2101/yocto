8fb85c9b31cc30368df6828dcda6eb10e17229b1
vovan2101 <veliseykin2000@gmail.com>
2024-10-22 19:29:58 +0000

    fix

--- bot/src/routes/FormRequestRoutes.js
+++ bot/src/routes/FormRequestRoutes.js
@@ -1,7 +1,7 @@
 // routes/formRequestRoutes.js
 const express = require('express');
 const router = express.Router();
-const { saveFormRequest } = require('../controllers/formRequestController');
+const { saveFormRequest } = require('../controllers/FormRequestController');
 
 router.post('/', saveFormRequest);
 
