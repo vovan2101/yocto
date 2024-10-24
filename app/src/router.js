0e1420cf2e6abce73087d1210975f4e5965857c3
vovan2101 <veliseykin2000@gmail.com>
2024-10-24 19:00:37 +0000

    fix

--- app/src/router.js
+++ app/src/router.js
@@ -15,7 +15,7 @@ export default new Router({
   mode: 'history',
   routes: [
     {
-      path: '/come',
+      path: '*',
       name: 'ComingSoon',
       component: ComingSoon, // Отображаем только этот компонент для всех маршрутов
     },
