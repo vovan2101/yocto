9341c5e72ed36e35840fb26a67d19b27b104d442
vovan2101 <veliseykin2000@gmail.com>
2024-11-23 02:57:28 +0000

    fix

--- app/src/views/sendForm.vue
+++ app/src/views/sendForm.vue
@@ -3722,7 +3722,7 @@ async submitForm() {
 },
 initWebSocket() {
     // Подключаемся к WebSocket серверу
-    this.socket = new WebSocket(`ws://test.yocto.vc/ws`);
+    this.socket = new WebSocket(`ws://test.yocto.vc/api/ws`);
 
     this.socket.onopen = () => {
       console.log('WebSocket соединение установлено.');
