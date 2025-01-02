62936c40b72a0ef0309b0a4c88615306b9a60462
vovan2101 <veliseykin2000@gmail.com>
2025-01-02 05:47:06 +0000

    fix

--- bot/puppeteer/hustleFundForm.js
+++ bot/puppeteer/hustleFundForm.js
@@ -8,7 +8,7 @@ const fillhustleFundForm = async (formData) => {
         return;
     }
 
-    const MAX_ATTEMPTS = 3;
+    const MAX_ATTEMPTS = 6;
     let attempt = 0;
     let success = false;
     let browser;
